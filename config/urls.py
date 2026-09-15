84f003be77e7afb216f4d127e8e7044ef091e1b3
satyam singh <satyam.singh@csgtech.in>
2026-09-15 18:47:30 +0530

    Fix media image serving in production

--- config/urls.py
+++ config/urls.py
@@ -71,8 +71,9 @@ urlpatterns = [
 ]
 
 # Static & Media serving during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Custom Error Handlers
